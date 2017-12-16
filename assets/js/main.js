2a876c8b5d0501a29c4b700595d62232754273f5
Bishwjit <30503242+Bishwjit@users.noreply.github.com>
2017-12-17 01:01:55 +0600

    fashion

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+		$(".product-list").masonry();
+
+
+
+
+		$('.homepage-slider').owlCarousel({
+			items: 1,
+		    loop:true,
+		    nav:true,
+		    autoplay: true,
+		    navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"]
+		})
+
+
+
+
+});
