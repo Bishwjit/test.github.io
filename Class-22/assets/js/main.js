e4876ee3efcc24e5d1959f907d756e20936486c3
Bishwjit <30503242+Bishwjit@users.noreply.github.com>
2018-02-14 02:10:48 +0600

    Class-22

---
+++ Class-22/assets/js/main.js
@@ -0,0 +1,20 @@
+/*(function ($) {
+	"use strict";
+
+        $('#table_id').DataTable({
+
+        });
+        
+
+
+
+
+
+
+}(jQuery));	
+*/
+$(document).ready( function () {
+    $('#table_id').DataTable({
+    	paging: true
+    });
+} );
